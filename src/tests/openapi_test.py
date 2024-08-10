e363d3983407a0da11f79f83c211b8dab8e40635
Guy Ronen <guy.ronen@codility.com>
2024-08-10 17:33:42 +0200

    last fix

--- src/tests/openapi_test.py
+++ src/tests/openapi_test.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from yaml import safe_load
+import yaml
 
 from smart_cart.main import app
 
@@ -8,7 +8,7 @@ from smart_cart.main import app
 def test_openapi_spec():
     src_path = Path(__file__).parent.parent
     with open(f"{src_path}/openapi.yml") as file:
-        stored_spec = safe_load(file)
+        stored_spec = yaml.safe_load(file)
 
     current_spec = app.openapi()
 
