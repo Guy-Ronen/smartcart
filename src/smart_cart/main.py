0d6fa75645fe799ac72aa7e22d9efa990fb862a0
Guy Ronen <guy.ronen@codility.com>
2024-08-10 15:09:40 +0200

    chore: Add TokenMiddleware to FastAPI app

--- src/smart_cart/main.py
+++ src/smart_cart/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from smart_cart.routers.health import router as health_router
+from smart_cart.utils.middleware import TokenMiddleware
 
 app = FastAPI()
 
 app.include_router(health_router)
+
+app.add_middleware(TokenMiddleware)
