82da9899d815956fcafce59ccb3b09f7773e900c
Guy Ronen <guy.ronen@codility.com>
2024-08-14 08:57:25 +0200

    created a user factory

--- src/smart_cart/utils/factories.py
+++ src/smart_cart/utils/factories.py
@@ -2,6 +2,7 @@ import datetime
 import uuid
 
 from smart_cart.schemas.token import TokenPayload
+from smart_cart.schemas.user import User
 
 
 def token_payload_factory(
@@ -19,4 +20,34 @@ def token_payload_factory(
         email=email,
         created_at=created_at,
         expires_at=expires_at,
+    )
+
+
+def user_factory(
+    id: str = str(uuid.uuid4()),
+    username: str = "john_doe",
+    email: str = "john.doe@example.com",
+    hashed_password: str = "hashed_password",
+    first_name: str = "John",
+    last_name: str = "Doe",
+    created_at: int = int(datetime.datetime.now().timestamp()),
+    updated_at: int = None,
+    last_login: int = None,
+    is_active: bool = True,
+    is_superuser: bool = False,
+    is_staff: bool = False,
+):
+    return User(
+        id=id,
+        username=username,
+        email=email,
+        hashed_password=hashed_password,
+        first_name=first_name,
+        last_name=last_name,
+        created_at=created_at,
+        updated_at=updated_at,
+        last_login=last_login,
+        is_active=is_active,
+        is_superuser=is_superuser,
+        is_staff=is_staff,
     )
