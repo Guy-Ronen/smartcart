4d7ef375e6a3cbcbf843bce8b4ca7f2fa8ada157
Guy Ronen <guy.ronen@codility.com>
2024-08-15 09:46:27 +0200

    feat: Update User model with user_id field and from_dynamoItem method

--- src/smart_cart/schemas/user.py
+++ src/smart_cart/schemas/user.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, EmailStr
 
 
 class User(BaseModel):
-    id: str = str(uuid.uuid4())
+    user_id: str = str(uuid.uuid4())
     username: str
     email: EmailStr
     hashed_password: str
@@ -18,3 +18,20 @@ class User(BaseModel):
     is_active: bool = True
     is_superuser: bool = False
     is_staff: bool = False
+
+    @classmethod
+    def from_dynamoItem(cls, item: dict) -> "User":
+        return cls(
+            user_id=item["user_id"],
+            username=item["username"],
+            email=item["email"],
+            hashed_password=item["hashed_password"],
+            first_name=item["first_name"],
+            last_name=item["last_name"],
+            created_at=item["created_at"],
+            updated_at=item.get("updated_at"),
+            last_login=item.get("last_login"),
+            is_active=item["is_active"],
+            is_superuser=item["is_superuser"],
+            is_staff=item["is_staff"],
+        )
