0102293044b5d8f01719b1d4df127fe6125ce0f0
Guy Ronen <guy.ronen@codility.com>
2024-08-15 09:55:48 +0200

    chore: Include root router in main.py

--- src/smart_cart/main.py
+++ src/smart_cart/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
+from smart_cart.routers.root import router as root_router
 from smart_cart.routers.health import router as health_router
 from smart_cart.utils.middleware import TokenMiddleware
 
 app = FastAPI()
 
+app.include_router(root_router)
 app.include_router(health_router)
 
 app.add_middleware(TokenMiddleware)
