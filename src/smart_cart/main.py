3ae0f2f98fcf94614c1e14a17d8e5b0164490515
Guy Ronen <guy.ronen@codility.com>
2024-08-20 10:21:35 +0200

    added route to main

--- src/smart_cart/main.py
+++ src/smart_cart/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from smart_cart.routers.health import router as health_router
 from smart_cart.routers.root import router as root_router
+from smart_cart.routers.signup import router as signup_router
 from smart_cart.utils.middleware import TokenMiddleware
 
 app = FastAPI()
 
 app.include_router(root_router)
 app.include_router(health_router)
+app.include_router(signup_router, prefix="/api/v1")
 
 app.add_middleware(TokenMiddleware)
