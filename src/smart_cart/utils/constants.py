e2318a44eb8c1b693f746d4204b23879ee3ac878
Guy Ronen <guy.ronen@codility.com>
2024-08-21 09:29:31 +0200

    feat: Add datetime constants for current time and timestamp

--- src/smart_cart/utils/constants.py
+++ src/smart_cart/utils/constants.py
@@ -1,3 +1,5 @@
+import datetime
+
 WHITELISTED_ENDPOINTS = [
     "/health",
     "/redoc",
@@ -5,3 +7,6 @@ WHITELISTED_ENDPOINTS = [
     "/api/v1/signup",
     "/api/v1/login",
 ]
+
+DATETIME_NOW = datetime.datetime.now(datetime.UTC)
+DATETIME_NOW_TIMESTAMP = int(datetime.datetime.now(datetime.UTC).timestamp())
