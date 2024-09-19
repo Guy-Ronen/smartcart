4ebbf469b79a3f3d0977c88fae5cb914a9bdc039
Guy Ronen <guy.ronen@codility.com>
2024-09-19 09:25:01 +0200

    Refactor token_payload_factory in factories.py

---
+++ src/smart_cart/factories/token.py
@@ -0,0 +1,24 @@
+import datetime
+from typing import Optional
+import uuid
+
+from smart_cart.models.token import TokenPayload
+from smart_cart.utils.constants import DATETIME_NOW, DATETIME_NOW_TIMESTAMP
+
+
+def token_payload_factory(
+    jti: Optional[str] = None,
+    sub: str = "user123",
+    iat: Optional[int] = None,
+    exp: Optional[int] = None,
+):
+    jti = jti or str(uuid.uuid4())
+    iat = iat or DATETIME_NOW_TIMESTAMP
+    exp = exp or int((DATETIME_NOW + datetime.timedelta(days=1)).timestamp())
+
+    return TokenPayload(
+        jti=jti,
+        sub=sub,
+        iat=iat,
+        exp=exp,
+    )
