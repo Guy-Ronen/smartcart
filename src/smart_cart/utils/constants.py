5d65dc09c2b3bb49cd0bfe3099575ecb5de9c2f8
Guy Ronen <guy.ronen@codility.com>
2024-11-18 07:31:29 +0000

    Add user-related constants and bcrypt hashing for fixed credentials

--- src/smart_cart/utils/constants.py
+++ src/smart_cart/utils/constants.py
@@ -1,5 +1,7 @@
 import datetime
 
+import bcrypt
+
 WHITELISTED_ENDPOINTS = [
     "/health",
     "/redoc",
@@ -8,5 +10,13 @@ WHITELISTED_ENDPOINTS = [
     "/api/v1/login",
 ]
 
+# Datetime constants
 DATETIME_NOW = datetime.datetime.now(datetime.UTC)
 DATETIME_NOW_TIMESTAMP = int(datetime.datetime.now(datetime.UTC).timestamp())
+
+# User constants
+FIXED_USER_ID = "df96b767-107f-4789-abd9-b669ace362ea"
+FIXED_EMAIL = "user_fixed@example.com"
+FIXED_PASSWORD = "password"
+FIXED_HASHED_PASSWORD = bcrypt.hashpw(FIXED_PASSWORD.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+FIXED_TIMESTAMP = 1731311609
