96b716427e8350c4135b078defffe8d9c2f9550b
Guy Ronen <guy.ronen@codility.com>
2025-03-15 17:30:21 +0700

    Refactor parse_receipt.py to use Headers for upload_file and improve variable naming for clarity

--- src/scripts/parse_receipt.py
+++ src/scripts/parse_receipt.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from fastapi import UploadFile
+from starlette.datastructures import Headers
 
 from smart_cart.parser.processor import ReceiptProcessor
 from smart_cart.schemas.response import ResponseSchema
@@ -10,14 +11,14 @@ receipt_image = Path(__file__).parent.parent / "smart_cart/parser/img" / FILE_NA
 results_path = (Path(__file__).parent.parent / "smart_cart/parser/results" / FILE_NAME).with_suffix(".json")
 
 with open(receipt_image, "rb") as file:
-    upload_file = UploadFile(file=file, filename=FILE_NAME, headers={"content-type": "image/jpeg"})
+    upload_file = UploadFile(file=file, filename=FILE_NAME, headers=Headers({"content-type": "image/jpeg"}))
     processor = ReceiptProcessor(upload_file)
 
     print("Processing receipt...")
     result: ResponseSchema = processor.process_and_get_result()
 
-    with open(results_path, "w", encoding="utf-8") as file:
-        file.write(result.model_dump_json(indent=4))
+    with open(results_path, "w", encoding="utf-8") as json_file:
+        json_file.write(result.model_dump_json(indent=4))
 
     print(f"Receipt:\n{result.model_dump_json(indent=4)}")
     print(f"Results saved to: {results_path}")
