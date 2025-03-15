e3e79e8f2bd1edc8193439d7f0eee4fefaeb6678
Guy Ronen <guy.ronen@codility.com>
2025-03-15 17:25:45 +0700

    Add ReceiptProcessor class for handling receipt processing and result retrieval

---
+++ src/smart_cart/parser/processor.py
@@ -0,0 +1,47 @@
+import time
+
+import requests
+from fastapi import HTTPException, UploadFile
+
+from smart_cart.schemas.response import ResponseSchema
+from smart_cart.utils.settings import settings
+
+
+class ReceiptProcessor:
+    def __init__(self, image: UploadFile):
+        self.image = image
+        self.process_endpoint = settings.tabs_scanner_process_endpoint
+        self.result_endpoint = settings.tabs_scanner_result_endpoint
+        self.headers = {"apikey": settings.tabs_scanner_api_key}
+
+    def call_process(self):
+        payload = {"documentType": "receipt", "decimalPlaces": "2", "region": "de"}
+
+        files = {"file": (self.image.filename, self.image.file, self.image.content_type)}
+        response = requests.post(self.process_endpoint, files=files, data=payload, headers=self.headers)
+        result = response.json()
+
+        if result.get("status") == "success" and result.get("token"):
+            return result["token"]
+        raise HTTPException(status_code=400, detail=f"Error processing receipt: {result}")
+
+    def call_result(self, token):
+        url = f"{self.result_endpoint}/{token}"
+
+        while True:
+            response = requests.get(url, headers=self.headers)
+            result = response.json()
+
+            if result.get("status") == "done" and "result" in result:
+                return ResponseSchema(**result)
+            elif result.get("status") == "error":
+                raise HTTPException(status_code=400, detail=f"Error retrieving result: {result}")
+
+            time.sleep(1)
+
+    def process_and_get_result(self):
+        try:
+            token = self.call_process()
+            return self.call_result(token)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
